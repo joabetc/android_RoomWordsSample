e6781d8a2dd281384f15d9ce61cd991d0fc1e591
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-12-06 15:27:55 -0300

    Adding RecyclerView to MainActivity

--- app/src/main/java/com/joabe/roomwordssample/MainActivity.java
+++ app/src/main/java/com/joabe/roomwordssample/MainActivity.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.joabe.roomwordssample.adapter.WordListAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 import android.view.Menu;
@@ -29,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final WordListAdapter adapter = new WordListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
